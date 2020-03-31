368fa6e80fe0b40b56486d6d29f8ba4503299d8c
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2020-03-30 22:50:20 -0700

    Actualizado el Exp 1
    Se actualizó el experimento 1. Se separó la sección de platica del experimento 3, para hacerlo extensivo a los demás experimentos

--- exp3.js
+++ exp3.js
@@ -1,14 +1,9 @@
 var SocialRobot = require('./social_robot');
 var credentials = require('./config-services');
-var send = require('./app');
 var gn = require('./getname');
 var PhoneticSpanish = require('./PhoneticSpanish');
 var preguntas = require('./exp3qaa');
-
-var joy = { anim: 'joy', bcolor: '', speed: 2.0 };
-var sad = { anim: 'sad', bcolor: '', speed: 2.0 };
-var anger = { anim: 'anger', bcolor: '', speed: 2.0 };
-var ini = { anim: 'ini', bcolor: '', speed: 2.0 };
+var p = require('./platica');
 
 function procesar(texto) {
     return texto;
@@ -19,71 +14,46 @@ module.exports = {
         var social = new SocialRobot(credentials.config, credentials.credentials);
         var pregunta = preguntas.getPreguntas();       
         var nombre = await gn.getName(social, evaId, usuarioId);
+        //var aux = await p.inicial(social, evaId, usuarioId);
         social.emotions('ini', 0);
-        send.enviarMensaje(evaId, 'Explicación');
+        social.templog(evaId, 'Explicación');
         var obj = await social.play('./exp3files/explicacion2.wav');
     
         var i = 0;
         var correctas = 0;
+        var total = 0;
         var next = true;
         do {
     
             for (let j = i; j < i + 3; j++) {
                 do {
+                    social.templog(evaId, 'Pregunta: ' + pregunta[j].pregunta);
                     var obj = await social.play(pregunta[j].audio);
                     respuestaParticipante = await social.sendAudioGoogleSpeechtoText2(procesar);
                     social.stopListening();
                 } while (respuestaParticipante.includes('repite') || respuestaParticipante.includes('repetir'));
-                send.enviarMensaje(usuarioId, respuestaParticipante + ' --- ' + pregunta[j].respuesta);
-                send.enviarMensaje(usuarioId, PhoneticSpanish(respuestaParticipante) + ' --- ' + PhoneticSpanish(pregunta[j].respuesta));
+                social.templog(usuarioId, respuestaParticipante + ' --- ' + pregunta[j].respuesta);
+                social.templog(usuarioId, PhoneticSpanish(respuestaParticipante) + ' --- ' + PhoneticSpanish(pregunta[j].respuesta));
                 if (respuestaParticipante.includes('no')) {
                     var obj = await social.play(pregunta[j].respaudio);
                 } else if (pregunta[j].analyze) {
                     var expression = Analyze(pregunta[j].respuesta, respuestaParticipante);
-                    switch(expression) {
-                        case 2:
-                            social.emotions('joy', 0);
-                            var obj = await social.play(RespuestaCorrecta());
-                            social.emotions('ini', 0);
-                            break;
-                        case 1:
-                            social.emotions('sad', 1);
-                            var obj = await social.play('./exp3files/incompleta.wav');
-                            var obj = await social.play(pregunta[j].respaudio);
-                            social.emotions('ini', 0);
-                            break;
-                        case 0:
-                            social.emotions('sad', 1);
-                            var obj = await social.play('./exp3files/parcialmentecorrecta.wav');
-                            var obj = await social.play(pregunta[j].respaudio);
-                            social.emotions('ini', 0);
-
-                            break;
-                        default:
-                            social.emotions('sad', 1);
-                            var obj = await social.play(RespuestaIncorrecta());
-                            var obj = await social.play(pregunta[j].respaudio);
-                            social.emotions('ini', 0);
-                      }
+                    await Respuesta(social, evaId, pregunta[j], expression);
+                    if (expression > 0) {
+                        correctas++;
+                    }
                 } else if (respuestaParticipante.includes(pregunta[j].respuesta) || PhoneticSpanish(respuestaParticipante).includes(PhoneticSpanish(pregunta[j].respuesta))
                 || pregunta[j].respuesta.includes(respuestaParticipante) || PhoneticSpanish(pregunta[j].respuesta).includes(PhoneticSpanish(respuestaParticipante))) {
-                    send.eyes(joy);
-                    console.log('Correcta');
-                    send.enviarMensaje(evaId, 'Correcta');
+                    await Respuesta(social, evaId, pregunta[j], 2);
                     correctas++;
-                    var obj = await social.play(RespuestaCorrecta());
                 } else {
-                    send.eyes(sad);
-                    console.log('Incorrecta');
-                    send.enviarMensaje(evaId, 'Incorrecta');
-                    var obj = await social.play(RespuestaIncorrecta());
-                    var obj = await social.play(pregunta[j].respaudio);
+                    await Respuesta(social, evaId, pregunta[j], -1);
                 }
-                send.eyes(ini);
+                social.emotions('ini', 0);
             }
             i=i+3;
             if (correctas == 3) {
-                social.emotions('joy', 1);
+                social.emotions(( total == 6 ? 'surprise' : 'joy' ), 1);
                 var obj = await social.play('./exp3files/resumen1.wav');
             } else if (correctas == 2) {
                 social.emotions('joy', 0);
@@ -96,11 +66,12 @@ module.exports = {
                 var obj = await social.play('./exp3files/resumen4.wav');
             }
             social.emotions('ini', 0);
+            total += correctas;
             correctas = 0;
             var obj = await social.play('./exp3files/otra.wav');
             var respuestaParticipante = await social.sendAudioGoogleSpeechtoText2(procesar);
             social.stopListening();
-            send.enviarMensaje(evaId, respuestaParticipante);
+            social.templog(evaId, respuestaParticipante);
             if (respuestaParticipante.includes("no")) {
                 social.emotions('sad', 1);
                 next = false;
@@ -111,19 +82,9 @@ module.exports = {
             }
             social.emotions('ini', 1);
         } while (next);
+        social.savelogs(nombre);
     }
 };
-
-// function Leds(params) {
-// 	ledsApp.kill();
-// 	ledsApp = spawn('./leds/stop');
-// 	ledsApp = spawn('./leds/' + params);
-// }
-
-// function LedStop() {
-// 	ledsApp.kill();
-// 	ledsApp = spawn('./leds/stop');
-// }
 
 function RespuestaCorrecta() {
 	var array = ['./exp3files/correcta1.wav', './exp3files/correcta2.wav', './exp3files/correcta3.wav'];
@@ -154,6 +115,37 @@ function Analyze(respuesta, participante) {
 	} else {
 		return -1;
 	}
+}
+
+async function Respuesta(social, evaId, pregunta, expression) {
+    switch(expression) {
+        case 2:
+            social.emotions('joy', 0);
+            social.templog(evaId, 'Correcta');
+            var obj = await social.play(RespuestaCorrecta());
+            social.emotions('ini', 0);
+            break;
+        case 1:
+            social.emotions('sad', 1);
+            social.templog(evaId, 'Incompleta');
+            var obj = await social.play('./exp3files/incompleta.wav');
+            var obj = await social.play(pregunta.respaudio);
+            social.emotions('ini', 0);
+            break;
+        case 0:
+            social.emotions('sad', 1);
+            social.templog(evaId, 'Parcialmente Correcta');
+            var obj = await social.play('./exp3files/parcialmentecorrecta.wav');
+            var obj = await social.play(pregunta.respaudio);
+            social.emotions('ini', 0);
+            break;
+        default:
+            social.emotions('sad', 1);
+            social.templog(evaId, 'Incorrecta');
+            var obj = await social.play(RespuestaIncorrecta());
+            var obj = await social.play(pregunta.respaudio);
+            social.emotions('ini', 0);
+      }
 }
 
 var generarNumeroRandom = function (min, max) {
