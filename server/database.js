2754ca054f7cd268947c97243d0e96330eb5de15
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2020-05-19 01:25:03 -0700

    Seguridad

--- server/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb+srv://amitjans:8KNysUAqsPqco3mO@cluster0-sniue.gcp.mongodb.net/interaccion';
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(db => console.log('Conectado a la Base de Datos'))
-    .catch(error => console.error(error));
-module.exports = mongoose;
