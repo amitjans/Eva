62ca368e6e8f038f6a09970ff4cb950d7f09fe3b
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2020-06-10 13:43:13 -0700

    Mejoreas
    Mejoras al obtener el nombre y simplificación de la condición de repetir.

--- interacciones/common/getname.js
+++ interacciones/common/getname.js
@@ -1,27 +1,33 @@
-function ProcesarNombre (nombre){
+Array.prototype.unique = function (a) {
+    return function () { return this.filter(a) }
+}(function (a, b, c) {
+    return c.indexOf(a, b + 1) < 0
+});
+
+function ProcesarNombre(nombre) {
     if (nombre.includes(" ")) {
         //return nombre.split(" ").pop();
         var aux = nombre.split(" ");
-        for (let i = aux.length - 1; i >= 0 ; i--) {
+        for (let i = aux.length - 1; i >= 0; i--) {
             if (/(^[A-Za-z]{1,2}$|nombre|llamo|hola|Hola)/.test(aux[i])) {
                 aux.splice(i, 1);
             }
         }
         if (aux.length > 1) {
-            for (let i = aux.length - 1; i >= 0 ; i--) {
+            for (let i = aux.length - 1; i >= 0; i--) {
                 if (aux[i][0] !== aux[i][0].toUpperCase()) {
                     aux.splice(i, 1);
-                }                
+                }
             }
         }
-        return aux.join(' ');
+        return aux.unique().join(' ');
     } else {
         return nombre;
     }
 }
 
 module.exports = {
-    getName : async function (social, evaId, usuarioId) {
+    getName: async function (social, evaId, usuarioId) {
         social.emotions('ini', 0);
         social.templog(evaId, 'Hola');
         var obj = await social.play('./interacciones/exp3files/hola.wav');
