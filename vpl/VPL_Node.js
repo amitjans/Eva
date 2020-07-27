898dd0ca0e1fe2af27e1db074827b1010f4b6592
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2020-07-26 20:40:45 -0700

    Agregado nodo para consumir servicios rest v1

--- vpl/VPL_Node.js
+++ vpl/VPL_Node.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 
 var random = require('../utils/Random');
 var lf = require('./ListeningFilters');
-var nodeutils = require('./NodeUtils');
+const api = require('./Api_Node');
 var app = require('../app');
 
 module.exports = {
@@ -34,7 +34,19 @@ module.exports = {
             }
         } else if (element.type === 'counter') {
             ProcessCounterNode(element);
-        }
+        } else if (element.type === 'api') {
+            let pos = element.host.indexOf(':');
+            if (pos >= 0) {
+                element.host = element.host.substring(pos + 3);
+            }
+            if (element.path.includes('$')) {
+                element.path = replaceWhatWasHeard(element.path);
+                console.log(element.path);
+            }
+            let data = await api.Api(element.version + '://' + element.host, element.path, element.port);
+            app.setApi(element.name.toLowerCase(), data);
+		    console.log('respuesta: ' + api.Getdata(app.getApi(element.name.toLowerCase()), 'gender'));
+        } 
     }
 };
 
@@ -51,31 +63,35 @@ function ProcessEmotionNode(social, element) {
 }
 
 async function ProcessSpeakNode(social, evaId, element) {
-    social.templog(evaId, element.text);
     let t = element.text;
     if (t.includes('/')) {
         t = random.getOne(t.split('/'));
         element.key = crypto.createHash('md5').update(t).digest("hex");
         element.text = t;
     }
-
-    if (t.includes('$')) {
-        t = nodeutils.includeAnswers(t.split(' '), app.getRespuesta());
-        await social.speak(t, element.anim, !element.anim);
-    } else if(t.includes('%')) {
-        let temp = (/^(%|%2)$/.test(t) ? app.getSactual().campo2 : app.getSactual().campo1);
-        await RecAndSpeak(social, evaId, { key: crypto.createHash('md5').update(temp).digest("hex"), type: "speak", text: temp });
-    } else if(t.includes('#')) {
-        let temp = t.split(' ');
-        for (let i = 0; i < temp.length; i++) {
+    let rec = true;
+    social.templog(evaId, element.text);
+    let temp = element.text.split(' ');
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i].includes('@')) {
+            let pos = temp[i].indexOf('.');
+            temp[i] = api.Getdata(app.getApi(temp[i].substring(1, pos).toLowerCase()), temp[i].substring(pos + 1));
+        } else if (temp[i].includes('$')) {
+            rec = false;
+            temp[i] = replaceWhatWasHeard(temp[i]);
+        } else if(temp[i].includes('%')) {
+            temp[i] = (/^(%|%2)$/.test(temp[i]) ? app.getSactual().campo2 : app.getSactual().campo1);
+        } else if(temp[i].includes('#')) {
             if (/^#[\w\d]+$/.test(temp[i])) {
                 temp[i] = app.getCounter()[temp[i].substring(1)];
             }
         }
-        await social.speak(temp.join(' '), element.anim, !element.anim);
-    } else {
-        await RecAndSpeak(social, evaId, element);
     }
+    if (rec) {
+        await RecAndSpeak(social, evaId, { key: crypto.createHash('md5').update(temp.join(' ')).digest("hex"), type: "speak", text: temp.join(' ') });
+    } else {
+        await social.speak(temp.join(' '), element.anim, !element.anim);
+    } 
 }
 
 async function RecAndSpeak(social, evaId, element) {
@@ -114,4 +130,16 @@ function ProcessCounterNode(element) {
         temp[element.count] = parseInt(element.value);
     }
     app.setCounter(temp);
+}
+
+function replaceWhatWasHeard(value) {
+    let respuesta = app.getRespuesta();
+    if (/\$-[\d]+/.test(value)) {
+        value = respuesta[(respuesta.length - 1) - parseInt(value.substring(2))];
+    } else if (/\$[\d]+/.test(value)) {
+        value = respuesta[parseInt(value.substring(1)) - 1];
+    } else {
+        value = value.replace('$', respuesta[respuesta.length - 1]);
+    }
+    return value;
 }
