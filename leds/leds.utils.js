18ade673b8292d7b566e918f30dd8570a093f373
amitjans <adrian.mitjans@gmail.com>
2021-04-16 18:41:47 -0700

    Cambios menores en algunas animaciones
    Se realizaron cambios en algunas animaciones. Se corrigió el error que impedia que se ejecutara correctamente la animación de hablar por defecto.

--- leds/leds.utils.js
+++ leds/leds.utils.js
@@ -11,7 +11,7 @@ function frente (value) {
 }
 
 function hexToRgb(value) {
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value.toLowerCase());
     return { r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16) }
 }
 
