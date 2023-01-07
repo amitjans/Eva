854c7bf734a11f63cbec78554960c054d8530a96
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2023-01-07 15:06:32 -0800

    Limpiando archivo index de ca carpeta de leds.

--- leds/index.js
+++ leds/index.js
@@ -1,15 +1,3 @@
-// const { escuchaC, escuchaT, stop } = require('./leds.speakAndListen');
-// const { anger, joy, sad } = require('./leds.emotions');
-
-// module.exports = {
-//     escuchaC,
-//     escuchaT,
-//     anger,
-//     joy,
-//     sad,
-//     stop
-// }
-
 require('fs').readdirSync(__dirname + '/').forEach(function (file) {
     if (file.match(/\.js$/) !== null && file !== 'index.js' && file !== 'leds.utils.js') {
         Object.assign(module.exports, require('./' + file));
